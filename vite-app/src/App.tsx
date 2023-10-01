0c32e77a2d86bc3faa8640db0d380c1bdc7459b2
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-01 17:42:28 -0400

    fix: gitignore updated

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,14 +1,17 @@
-//import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup/ListGroup";
-//if we dont supply folder it looks for index.ts
+import Button from "./components/Buttons/button";
 
-function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+const App = () => {
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <Button
+        onClick={function () {
+          console.log("Hi Mom!");
+        }}
+      >
+        My-Button
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
