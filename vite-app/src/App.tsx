61ad61383cd546c632820a81d24723ec78144cfb
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-01 01:25:01 -0400

    feat: Added alert and close

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,11 +1,17 @@
 //import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
 
 import Button from "./components/button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false); //here the initial state if akert is hidden or false
   return (
     <div>
-      <Button color="secondary" onClick={() => console.log("clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisible(true)}>
         My-Button
       </Button>
     </div>
--- vite-app/src/components/Alert.tsx
+++ vite-app/src/components/Alert.tsx
@@ -2,10 +2,24 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  //to make the parent of the component we have clicjked the close button
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible ">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        data-bs-target="#my-alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
