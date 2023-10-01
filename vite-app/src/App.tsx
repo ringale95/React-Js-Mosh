bc2ad7c5212f9c840b5ae67513e5cf9eb86bcdd2
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-01 12:22:05 -0400

    feat: Added listgroup module and styling

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,19 +1,12 @@
-//import ListGroup from "./components/ListGroup";
-import { useState } from "react";
-import Alert from "./components/Alert";
-
-import Button from "./components/button";
+//import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup/ListGroup";
+//if we dont supply folder it looks for index.ts
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false); //here the initial state if akert is hidden or false
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
-      )}
-      <Button color="secondary" onClick={() => setAlertVisible(true)}>
-        My-Button
-      </Button>
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
