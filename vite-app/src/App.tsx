cfacd45992d1e72870cf531efec58aeb90f745dd
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-02 23:15:10 -0400

    feat: Updated quantity

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: " Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: [
+        ...cart.items.map((item) =>
+          item.id == 1 ? { ...item, quantity: 2 } : item
+        ),
+      ],
+    });
   };
+
   return (
     <div>
-      Toppings:
-      <ul>
-        {pizza.toppings.map((topping) => (
-          <li>{topping}</li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Add Cheese</button>
+      Quantity: {cart.items[0].quantity}
+      <button onClick={handleClick}>Update Quantity</button>
     </div>
   );
 };
