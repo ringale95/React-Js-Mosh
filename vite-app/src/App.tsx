5509df2f6310a7cdc2387f8c27774375bedbe305
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-04 09:52:40 -0400

    Handling form submission

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
 import Expandable from "./components/Expandable";
+import Forms from "./components/Forms";
 
 const App = () => {
   return (
     <div>
-      <Expandable maxCharLength={20}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-        cupiditate impedit, et id vitae iusto minima libero? Autem animi
-        perferendis quam modi iste id libero vel deleniti corporis debitis, enim
-        est suscipit fugiat officia earum necessitatibus nobis cum recusandae
-        numquam exercitationem, ex praesentium, sed deserunt! Odio corrupti
-        deserunt porro mollitia?
-      </Expandable>
+      <Forms />
     </div>
   );
 };
