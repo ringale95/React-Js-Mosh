443363be52ba9ed1b5d2b1b29730cb6eca83dd56
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-12 14:43:17 -0400

    Added forms-hook

--- vite-app/src/components/Forms.tsx
+++ vite-app/src/components/Forms.tsx
@@ -1,27 +1,19 @@
-import { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
+  const { register, handleSubmit } = useForm(); //destructure of form element
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    /*<form onSubmit={handleSubmit((data) => console.log(data))}>*/
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
@@ -32,10 +24,7 @@ const Form = () => {
             Age
           </label>
           <input
-            value={person.age}
-            onChange={(event) =>
-              setPerson({ ...person, age: parseInt(event.target.value) })
-            }
+            {...register("age")}
             id="age"
             type="number"
             className="form-control"
