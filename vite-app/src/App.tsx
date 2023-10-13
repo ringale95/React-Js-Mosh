ac4547c30fbb784c91f791bb1a1f7c3fbea3ec4d
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-13 19:37:32 -0400

    Added: disconnect and connect

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,24 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
 
 const App = () => {
-  const [category, setCategory] = useState("");
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => {
-          setCategory(event.target.value);
-        }}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <br />
-      <ProductList category={category} />
-    </div>
-  );
+  const connect = () => console.log("connecting");
+  const disconnect = () => console.log("disconnecting");
+  useEffect(() => {
+    connect();
+
+    //disconnect below for whatever connected above//
+    return () => disconnect();
+  });
+  return <div></div>;
 };
 
 export default App;
