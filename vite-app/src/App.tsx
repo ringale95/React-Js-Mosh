07b7fddb020ba0f6b212cb2c09012f61a1e3107c
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-14 19:03:02 -0400

    Updated user

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -62,11 +62,25 @@ const App = () => {
       });
   };
 
+  const updateUser = (user: User) => {
+    const originalUser = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUser);
+      });
+  };
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border "></div>}
-
       <button className="btn btn-primary mb-3" onClick={addUser}>
         Add
       </button>
@@ -79,12 +93,20 @@ const App = () => {
             //d-flex: used to display flex
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>{" "}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>{" "}
+            </div>
           </li>
         ))}
       </ul>
