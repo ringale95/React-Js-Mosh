7f5ed28b6cf98d91a5e911a77d0d2e416f5a798d
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-14 17:18:55 -0400

    Added loader'

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -11,23 +11,30 @@ interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const Controller = new AbortController();
+    setLoading(true);
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers", {
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: Controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
     return () => Controller.abort();
   }, []);
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border "></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
