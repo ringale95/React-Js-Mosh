e179c27ce0bfc79cfedd4808c91064788a898732
Raveena Ravindra Ingale <ingale.r@northeastern.edu>
2023-10-14 18:08:44 -0400

    Added delete functionality

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -31,13 +31,40 @@ const App = () => {
       });
     return () => Controller.abort();
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUser = [...users];
+    //update UI first and then call server:optimistic
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    //call server
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUser); //if error is caught set back to original users
+      });
+  };
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border "></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+            //justify content-between is used to distribute space between items
+            //d-flex: used to display flex
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>{" "}
+          </li>
         ))}
       </ul>
     </>
